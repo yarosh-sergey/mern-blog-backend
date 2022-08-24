5179968d0866666e8eb347d388fd3dcf1d944077
sergey.yarosh <s.yarosh@madbrains.ru>
2022-08-24 17:17:12 +0300

    fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import { handleValidationsErrors, checkAuth } from './utils/index.js';
 import cors from 'cors';
 
 mongoose
-  .connect('mongodb+srv://admin:admin@cluster0.qrprnjt.mongodb.net/?retryWrites=true&w=majority')
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
