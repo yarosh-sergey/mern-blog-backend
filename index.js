06d0858de2e29f1df1750c9e4edf3393f2b62d93
sergey.yarosh <s.yarosh@madbrains.ru>
2022-08-24 17:38:37 +0300

    fix

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ import {
 } from './controllers/index.js';
 import { handleValidationsErrors, checkAuth } from './utils/index.js';
 import cors from 'cors';
+import timeout from 'connect-timeout';
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -37,6 +38,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+app.use(timeout('15s'))
 app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
